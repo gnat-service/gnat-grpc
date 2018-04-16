1b27b62c80558b4cdbb3f151fbf868317d7b4c94
Chen Wen <joc98@163.com>
2018-04-16 10:04:36 +0800

    Update bin/install_protobuf.sh

--- bin/install_protobuf.sh
+++ bin/install_protobuf.sh
@@ -7,6 +7,7 @@ PROTOC_ZIP=$PROTOC_RELEASE.zip
 curl -OL https://github.com/google/protobuf/releases/download/$PROTOC_VER/$PROTOC_ZIP
 mkdir $PROTOC_RELEASE
 
+rm -rf /usr/local/bin/protoc /usr/local/include/google
 sudo unzip $PROTOC_ZIP -d $PROTOC_RELEASE/
 sudo cp $PROTOC_RELEASE/bin/protoc /usr/local/bin/
 sudo cp -a $PROTOC_RELEASE/include/google /usr/local/include/
